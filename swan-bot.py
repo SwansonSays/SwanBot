a978acfae65a024b6360646050818d6062e2ad42
SwansonSays <rpswanson18@gmail.com>
2024-02-15 17:43:23 -0800

    Started playing around with tracking amount of time in voice channel. Still need to figure out how I want to store the channel join time and access it when leave channel event happens

--- swan-bot.py
+++ swan-bot.py
@@ -2,6 +2,7 @@ import os
 import discord, logging
 from dotenv import load_dotenv
 from discord.ext import commands
+import datetime
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -13,10 +14,30 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+join_time = -1
+leave_time = -1
+
 #Event handling
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    #Logs to console on voice channel join
+    if before.channel == None and after.channel != None:
+        join_time = datetime.datetime.now()
+        print(f'{member} JOINED {before.channel} @ {join_time}')
+        print(f'Hello {member}!')
+
+    #Logs to console on voice channel leave
+    if before.channel != None and after.channel == None:
+        leave_time = datetime.datetime.now()
+        print(f'{member.name} LEFT {before.channel} @ {leave_time}')
+
+        #Sends msg to general text channel on voice channel leave
+        channel = bot.get_channel(784337067307302914)
+        await channel.send("HII")
 
 #Command handling
 @bot.command()
