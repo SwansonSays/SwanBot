d396ab4e870e6ceee18f5c7ddfd2f7a196f4a76c
SwansonSays <rpswanson18@gmail.com>
2024-02-15 16:15:56 -0800

    installed dotenv to protect token, updated .gitignore to ignore env, imported discord.ext commands for command handling

--- swan-bot.py
+++ swan-bot.py
@@ -1,22 +1,41 @@
-import discord
+import os
+import discord, logging
+from dotenv import load_dotenv
+from discord.ext import commands
 
-token = ''
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
-    
-    if message.content.startwith('$hello'):
+
+    if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(token)
+    await bot.process_commands(message)
+
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(f'This bitch said {arg}')
+
+@bot.command()
+async def echo(ctx, *args):
+    arguments = ' '.join(args)
+    await ctx.send(arguments)
+
+
+bot.run(token=TOKEN, log_handler=handler, log_level=logging.DEBUG)
